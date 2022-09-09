666d0bdf0f5aff4b8b55a2b8c131d9492dd5cfec
miko <rosalesmikole@gmail.com>
2022-09-09 15:17:29 +0800

    changes

--- personal-portfolio/src/components/Skills.js
+++ personal-portfolio/src/components/Skills.js
@@ -38,20 +38,20 @@ export const Skills = () => {
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter2} alt="Image" />
                                 <h5>Web Development</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <img src={meter3} alt="Image" />
+                                <h5>UI/UX Design</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
                                 <h5>Logo Design</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={meter2} alt="Image" />
+                                <h5>Brand Identify</h5>
                             </div>
                         </Carousel>
                     </div>
